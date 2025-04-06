48766a411413d58a80592339246abc798dfc7552
ricar <ricar@host.docker.internal>
2025-04-06 17:30:15 -0300

    Criação de moledos Imagem, Notas, Produto, Cupom, etc

--- src/main/java/com/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/com/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,10 @@ package com.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.mentoria.lojavirtual.model")
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
